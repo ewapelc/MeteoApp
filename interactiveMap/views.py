3ff8551c0eaa5bfa758b6dead8257b41d8bacd82
Ewa <ewapelc00@gmail.com>
2023-04-26 23:54:13 +0200

    Clip points to country borders

--- interactiveMap/views.py
+++ interactiveMap/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import Point, Polygon
 from django.contrib.gis.db.models.functions import Distance
-from .models import Map
+from .models import Map, WorldBorder
 
 latitude = 51.919438
 longitude = 19.145136
@@ -14,8 +14,16 @@ user_location = Point(longitude, latitude, srid=4326)
 class Home(generic.ListView):
     model = Map
     context_object_name = 'stations'
-    queryset = Map.objects.filter(latitude__gte=49, latitude__lte=55, longitude__gte=14, longitude__lte=24).values()
-    #queryset = queryset.annotate('pwat')
+    # country_borders = Polygon(WorldBorder.objects.filter(name='Poland').get('mpoly'))
+    # queryset = Map.objects.filter(latitude__gte=49, latitude__lte=55, longitude__gte=14, longitude__lte=24).values()
+    country = WorldBorder.objects.filter(name='Poland').values()
+    country_geometry = country[0]['mpoly']
+    queryset = Map.objects.filter(geometry__intersects=country_geometry)
+    # queryset = []
+    # for station in points:
+    #     p = Point(station.latitude, station.longitude)
+    #     queryset.append(country_borders.contains(p))
+    #queryset = Map.objects.annotate()
     template_name = "stations/index.html"
 
 
