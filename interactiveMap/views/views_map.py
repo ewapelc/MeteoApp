18e4823a87be60b3b14ecc57c59aaebb53537f49
Ewa <ewapelc00@gmail.com>
2023-08-03 18:53:09 +0200

    views_map.py code cleanup

--- interactiveMap/views/views_map.py
+++ interactiveMap/views/views_map.py
@@ -23,44 +23,64 @@ def map_page(request):
     if request.method != 'POST':
         m = folium.Map([0, 0], zoom_start=2)
     elif request.method == 'POST':
-        # store user selection as session variables
-        request.session['visual_type'] = request.POST.get('visual_type')
-        request.session['chosen_country'] = request.POST.get('country')
-        request.session['date'] = request.POST.get('date')
-        request.session['meteo_var'] = request.POST.get('meteo_var')
+        # save user selection
+        context['selected_type'] = request.POST.get('visual_type')
+        context['selected_date'] = request.POST.get('date')
+        context['selected_var'] = request.POST.get('meteo_var')
+        context['selected_country'] = WorldBorder.objects.filter(id=request.POST.get('country')).values().last()
 
-        context['selected_country'] = WorldBorder.objects.filter(id=request.session['country']).values().last()
+        datetime_obj = datetime.strptime((context['selected_date']), "%d-%m-%Y %H:%M")
+        points = Location.objects.filter(
+            time=datetime_obj,
+            geometry__intersects=context['selected_country']['mpoly']
+        ).values()
 
-        country_geometry = WorldBorder.objects.filter(name=context['selected_country']['name']).values().last()['mpoly']
-        datetime_obj = datetime.strptime((request.session['date']), "%d-%m-%Y %H:%M")
-        points = Location.objects.filter(time=datetime_obj, geometry__intersects=country_geometry).values()
-
-        context['count'] = points.count()
-
-        selected_var = request.session['meteo_var']
-        var_data = long_name_and_unit(selected_var)
+        context['points_count'] = points.count()
 
         # prepare queryset for timeseries plot
-        queryset = Location.objects.filter(geometry__intersects=country_geometry).values('time').annotate(
-            meteo_var=Avg(selected_var)).order_by()
+        queryset = Location.objects.filter(
+            geometry__intersects=context['selected_country']['mpoly']
+        ).values('time').annotate(
+            meteo_var=Avg(context['selected_var'])
+        ).order_by()
 
+        # time series data
         meteo_var = queryset.values_list('meteo_var', flat=True)
-        time = queryset.values_list('time', flat=True)
+        time1 = queryset.values_list('time', flat=True)
         timezone_corrected_l = [datetime.strptime(
             datetime.strftime(TIME + timedelta(hours=2), "%d-%m-%Y %H:%M"),
             "%d-%m-%Y %H:%M"
-        ) for TIME in time]
+        ) for TIME in time1]
+
+        # scatter point data
+        val = queryset.filter(
+            time=datetime_obj
+        ).values_list('meteo_var', flat=True)
+        time2 = queryset.filter(
+            time=datetime_obj
+        ).values_list('time', flat=True)
+        timezone_corrected = datetime.strptime(
+            datetime.strftime(time2[0] + timedelta(hours=2), "%d-%m-%Y %H:%M"),
+            "%d-%m-%Y %H:%M"
+        )
+
+        # calculate average value of chosen variable
+        avg = Location.objects.filter(
+            geometry__intersects=context['selected_country']['mpoly']
+        ).values('time').annotate(
+            meteo_var=Avg(context['selected_var'])
+        ).order_by().aggregate(avg=Avg('meteo_var'))['avg']
 
         fig = create_timeseries(
-            timezone_corrected_l,
-            meteo_var,
-            var_data,
-            context,
-            selected_var,
-            country_geometry,
-            queryset,
-            datetime_obj
+            time_list1=timezone_corrected_l,
+            var_list1=meteo_var,
+            time_list2=[timezone_corrected],
+            var_list2=list(val),
+            selected_country=context['selected_country'],
+            selected_var=context['selected_var'],
+            average_var=avg,
         )
+
         fig = fig._repr_html_()
         context['plot_div'] = fig
 
@@ -78,19 +98,19 @@ def map_page(request):
             attr='Esri.WorldImagery'
         ).add_to(m)
 
-        if request.session['visual_type'] == 'points':
+        if context['selected_type'] == 'points':
             render_polygons(
                 m=m,
                 points=points,
                 selected_country=context['selected_country'],
-                selected_var=selected_var
+                selected_var=context['selected_var']
             )
-        elif request.session['visual_type'] == 'choropleth':
+        elif context['selected_type'] == 'choropleth':
             render_choropleth(
                 m=m,
                 datetime_obj=datetime_obj,
                 selected_country=context['selected_country'],
-                selected_var=selected_var
+                selected_var=context['selected_var']
             )
 
         folium.LayerControl().add_to(m)
@@ -100,41 +120,35 @@ def map_page(request):
     return render(request, 'stations/index.html', context)
 
 
-def create_timeseries(timezone_corrected_l, meteo_var, var_data, context, selected_var, country_geometry, queryset, datetime_obj):
+def create_timeseries(time_list1, var_list1, time_list2, var_list2, selected_country, selected_var, average_var):
     """ Creates a time series graph for the Interactive Map page. """
+    var_data = long_name_and_unit(selected_var)
 
+    # points for all dates and times
     fig = px.line(
-        x=timezone_corrected_l,
-        y=meteo_var,
-        title=f'Average {var_data["long_name"]} [{var_data["unit"]}] in {context["selected_country"]["name"]}',
+        x=time_list1,
+        y=var_list1,
+        title=f'Average {var_data["long_name"]} [{var_data["unit"]}] in {selected_country["name"]}',
         width=900,
         height=250,
         labels={"x": "Date", "y": var_data['long_name'] + ' [' + var_data['unit'] + ']'},
         markers=True
     )
 
-    avg = Location.objects.filter(geometry__intersects=country_geometry).values('time'). \
-        annotate(meteo_var=Avg(selected_var)).order_by().aggregate(avg=Avg('meteo_var'))['avg']
+    # the horizontal line indicates the average for the full date and time range
+    fig.add_hline(y=average_var, line_width=3, line_dash="dash", line_color="red")
 
-    fig.add_hline(y=avg, line_width=3, line_dash="dash", line_color="red")
-
-    time = queryset.filter(time=datetime_obj).values_list('time', flat=True)
-    val = queryset.filter(time=datetime_obj).values_list('meteo_var', flat=True)
-
-    timezone_corrected = datetime.strptime(
-        datetime.strftime(time[0] + timedelta(hours=2), "%d-%m-%Y %H:%M"),
-        "%d-%m-%Y %H:%M"
-    )
-
+    # one point for the selected date and time
     fig.add_scatter(
-        x=[timezone_corrected],
-        y=list(val),
+        x=time_list2,
+        y=var_list2,
         marker=dict(size=10, color="red"),
         name='Selected date',
     )
 
-    # style the plot
-    fig.update_layout(margin=dict(b=30, l=30, r=30, t=30))
+    fig.update_layout(
+        margin=dict(b=30, l=30, r=30, t=30)
+    )
 
     return fig
 
@@ -204,11 +218,11 @@ def render_polygons(m, points, selected_country, selected_var):
     # create polygons from original points, save them to GeoDataFrame
     step = 0.125
     all_points = data_gdf.geometry.tolist()
-    all_poly = [Polygon([[first.x - step, first.y + step],
-                         [first.x + step, first.y + step],
-                         [first.x + step, first.y - step],
-                         [first.x - step, first.y - step],
-                         [first.x - step, first.y + step]]) for first in all_points]
+    all_poly = [Polygon([[p.x - step, p.y + step],
+                         [p.x + step, p.y + step],
+                         [p.x + step, p.y - step],
+                         [p.x - step, p.y - step],
+                         [p.x - step, p.y + step]]) for p in all_points]
 
     poly_gdf = gpd.GeoDataFrame({'geometry': all_poly}, crs="EPSG:4326")
     poly_gdf['variable'] = data_gdf[selected_var]
@@ -264,46 +278,56 @@ def render_polygons(m, points, selected_country, selected_var):
                box-shadow: 3px;
                 """,
         ),
-        ).add_to(m)
+    ).add_to(m)
 
 
 def render_choropleth(m, datetime_obj, selected_country, selected_var):
     """ Creates and adds to the map a Choropleth Map with a tooltip layer. """
 
-    queryset = Location.objects.filter(time=datetime_obj, geometry__intersects=selected_country['mpoly']).annotate(
+    # queryset, which contains the average value of the variable for each region
+    reg_avg_queryset = Location.objects.filter(
+        time=datetime_obj,
+        geometry__intersects=selected_country['mpoly']
+    ).annotate(
         region=Subquery(
-            CountryRegion.objects.filter(geometry__contains=OuterRef('geometry')).values('name')
+            CountryRegion.objects.filter(
+                geometry__contains=OuterRef('geometry')
+            ).values('name')
         )
-    ).values('region', selected_var)
+    ).values('region').annotate(
+        avg=Avg(selected_var)
+    ).order_by('region')
 
-    queryset = queryset.values('region').annotate(avg=Avg(selected_var))
+    # queryset, which contains only geometry columns for regions from the previous queryset
+    region_names = reg_avg_queryset.values_list('region', flat=True)
+    geometry_queryset = CountryRegion.objects.filter(
+        country_iso3=selected_country['iso3'],
+        name__in=region_names
+    ).order_by('name').values('geometry')
 
-    region_names = queryset.values_list('region', flat=True)
-
-    qs = CountryRegion.objects.filter(country_iso3=selected_country['iso3'],
-                                      name__in=region_names)\
-        .order_by('name').values('geometry')
-
-    world_gdf = gpd.GeoDataFrame(list(qs), crs='EPSG:4326')
-    wkt1 = world_gdf.geometry.apply(lambda x: x.wkt)
-    world_gdf = gpd.GeoDataFrame(world_gdf, geometry=gpd.GeoSeries.from_wkt(wkt1), crs='EPSG:4326')
-
-    data_df = pd.DataFrame(list(queryset))
+    # convert query sets to GeoDataFrames
+    data_df = pd.DataFrame(list(reg_avg_queryset))
     data_df.dropna(axis=0, inplace=True)
 
-    world_gdf['region'] = data_df['region']
-    world_gdf['avg'] = data_df['avg']
+    region_gdf = gpd.GeoDataFrame(list(geometry_queryset), crs='EPSG:4326')
+    wkt1 = region_gdf.geometry.apply(lambda x: x.wkt)
+    region_gdf = gpd.GeoDataFrame(region_gdf, geometry=gpd.GeoSeries.from_wkt(wkt1), crs='EPSG:4326')
+    region_gdf['region'] = data_df['region']
+    region_gdf['avg'] = data_df['avg']
 
-    geojson = world_gdf.to_json()
+    geojson = region_gdf.to_json()
 
-    create_choropleth(
+    # create Choropleth map and add it to the folium map
+    choropleth = create_choropleth(
         m=m,
         variable=selected_var,
         datetime_obj=datetime_obj,
         geo_data=geojson,
-        data=world_gdf
+        data=region_gdf
     )
+    choropleth.add_to(m)
 
+    # create tooltips geojson and add it to the folium map
     var_data = long_name_and_unit(selected_var)
     folium.features.GeoJson(
         geojson,
@@ -332,11 +356,11 @@ def render_choropleth(m, datetime_obj, selected_country, selected_var):
 
 
 def create_choropleth(m, variable, datetime_obj, geo_data, data):
-    """ Creates a Choropleth Map. """
+    """ Creates and returns a Choropleth Map. """
 
     var_data = long_name_and_unit(variable)
 
-    folium.Choropleth(
+    choropleth = folium.Choropleth(
         name="Choropleth",
         geo_data=geo_data,
         data=data,
@@ -346,4 +370,6 @@ def create_choropleth(m, variable, datetime_obj, geo_data, data):
         fill_opacity=0.7,
         line_opacity=0.2,
         legend_name=f"Average {var_data['long_name']} [{var_data['unit']}] on {datetime_obj}"
-    ).add_to(m)
+    )
+
+    return choropleth
